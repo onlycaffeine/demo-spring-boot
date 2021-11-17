3754753a1e07404bdac1ad0c9f9dbee16649757d
onlycaffeine <vanthuanjul2@gmail.com>
2021-11-17 09:43:08 +0700

    initial Signed-off-by: onlycaffeine

--- src/main/java/com/thuan/anular/spring/example/DemoSpringBoot/DemoSpringBootApplication.java
+++ src/main/java/com/thuan/anular/spring/example/DemoSpringBoot/DemoSpringBootApplication.java
@@ -1,25 +1,9 @@
 package com.thuan.anular.spring.example.DemoSpringBoot;
 
-import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.thuan.anular.spring.example.dto.Book;
-
-@SpringBootApplication
-@RestController
 public class DemoSpringBootApplication {
 	
-	@GetMapping("/findAllBooks")
-	
-	public List<Book> getBooks(){
-		return Stream.of(new Book(101, "Java", 999),
-				new Book(103, "Spring", 888)).collect(Collectors.toList());
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootApplication.class, args);
 	}
